b0d2949416aac6b4c5c9defb0ef2625412514ede
gomyo <engcraft1102@gmail.com>
2020-05-22 16:37:26 +0900

    feat : 한번에 출력

--- 2020.05월/2020.05.22/ParameterConstructor.java
+++ 2020.05월/2020.05.22/ParameterConstructor.java
@@ -9,10 +9,16 @@ public class ParameterConstructor {
         age = a;
         isHungry = i;
     }
+    // 한번에 출력이 안 될리가 있나! 한번에 출력해주는 메서드를 만들고 호출하면 된다!
+    void printinfo() {
+        System.out.println(name+age+isHungry);
+    }
     public static void main(String[] args) {
         // if you wanted to print parameterConstructor's value:
         ParameterConstructor pp = new ParameterConstructor("최준원",26,false);
         System.out.print(pp.name);
+
+        pp.printinfo();
         // Cuz there's no default constructor, so, the constructor ParameterConstructor() is undefined Error occured.
         // ParameterConstructor p = new ParameterConstructor();\
         
